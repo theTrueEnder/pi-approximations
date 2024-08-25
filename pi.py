bec58d25688a857ccecceaf1d4812f2e1a05f901
theTrueEnder <67336179+theTrueEnder@users.noreply.github.com>
2024-08-25 16:33:50 -0400

    Initial commit

--- pi.py
+++ pi.py
@@ -1,17 +1,36 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-xmax = 250_000
-yrange = 50_000
+xmax = 1_000_000
+yrange = 1.5
 
-def f(x):
+def sin(x):
     try:
-        return 1.0 / np.sin(x)
+        return np.sin(160*x)
+    except ZeroDivisionError:
+        return 0
+    
+def csc(x):
+    try:
+        return 1.0 / np.sin(160*x)
     except ZeroDivisionError:
         return 0
 
+def flr(x):
+    try:
+        return x - np.floor(x)
+    except ZeroDivisionError:
+        return 0
+
+def mul60(x):
+    return 160*x
+
+def diri(x):
+    return ...
+
 xpoints = np.arange(1, xmax)
-ypoints = np.apply_along_axis(f, 0, xpoints)
+ypoints = np.apply_along_axis(flr, 0, xpoints)
+# xpoints = np.apply_along_axis(mul60, 0, xpoints)
 
 print(xpoints)
 print(ypoints)
